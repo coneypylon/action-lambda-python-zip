9e29dd55223cfefb2609f1cadd079f6626277d30
Nicholas Boyko <coneypylon@gmail.com>
2025-10-21 15:53:56 -0400

    Update entrypoint.sh with platform

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ configure_aws_credentials(){
 install_zip_dependencies(){
 	echo "Installing and zipping dependencies..."
 	mkdir python
-	pip install --target=python -r "${INPUT_REQUIREMENTS_TXT}"
+	pip install --platform "${INPUT_PLATFORM}" --target=python -r "${INPUT_REQUIREMENTS_TXT}"
 	zip -r dependencies.zip ./python
 }
 
